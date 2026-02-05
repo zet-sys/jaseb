3a5803402f81af50be0824347136888efc294cda
zet-sys <sugihartoiwan745@gmail.com>
2026-02-05 09:05:01 +0700

    Change BOT_TOKEN in config.js
    Updated BOT_TOKEN to a new value.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "Isi_bot_token",
+  BOT_TOKEN: "8200123910:AAHJcP_s37L11RLKCys-dd_W7jDjptB-1RE",
   OWNER_IDS: ["8483832365"],
   CHANNEL_USERNAME: "@AboutZetzy",
   GROUP_USERNAME: "@grubpubliczetzy",
